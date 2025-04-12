2e77ae3ab170551cf560c949c50886b9ca130044
Chrystian Sales <chrystianps@gmail.com>
2025-04-12 17:42:49 -0400

    Remove commented section and update comment for the default Laravel route

--- routes/web.php
+++ routes/web.php
@@ -4,19 +4,8 @@ use Faker\Guesser\Name;
 use Illuminate\Support\Facades\Route;
 use Ramsey\Uuid\Type\Integer;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/teste', 'TesteController@teste'); /* apenas teste, nao faz parte do curso */
-/*
+/* Rota padrão do Laravel
 Route::get('/', function () {
     return view('welcome');
 });
