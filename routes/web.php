c7bb77133e846d2e4c8d6b2ef6651620afd3c012
Chrystian Sales <chrystianps@gmail.com>
2025-04-13 10:58:36 -0400

    Add missing documentation header for web routes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,17 @@ use Faker\Guesser\Name;
 use Illuminate\Support\Facades\Route;
 use Ramsey\Uuid\Type\Integer;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
 Route::get('/', 'PrincipalController@principal')->name('site.index');
 Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
 Route::get('/contato', 'ContatoController@contato')->name('site.contato');
