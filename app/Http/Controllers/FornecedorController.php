2bc948a4e8428359bac6c3040384130e8b0902f6
Chrystian Sales <chrystianps@gmail.com>
2025-04-14 18:01:45 -0400

    Add additional fornecedores and enhance DDD case handling in index view

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -30,6 +30,41 @@ class FornecedorController extends Controller
                 'ddd' => '32', //minas gerais
                 'telefone' => '987654321',
             ],
+            3 => [
+                'nome' => 'Fornecedor 4',
+                'status' => 'S',
+                'cnpj' => '12345678901234',
+                'ddd' => '21', //rio de janeiro
+                'telefone' => '987654321',
+            ],
+            4 => [
+                'nome' => 'Fornecedor 5',
+                'status' => 'N',
+                'cnpj' => '12345678901234',
+                'ddd' => '41', //parana
+                'telefone' => '987654321',
+            ],
+            5 => [
+                'nome' => 'Fornecedor 6',
+                'status' => 'S',
+                'cnpj' => '12345678901234',
+                'ddd' => '51', //rio grande do sul
+                'telefone' => '987654321',
+            ],
+            6 => [
+                'nome' => 'Fornecedor 7',
+                'status' => 'N',
+                'cnpj' => '12345678901234',
+                'ddd' => '61', //distrito federal
+                'telefone' => '987654321',
+            ],
+            7 => [
+                'nome' => 'Fornecedor 8',
+                'status' => 'S',
+                'cnpj' => '12345678901234',
+                'ddd' => '71', //bahia
+                'telefone' => '987654321',
+            ],
         ];
         return view('app.fornecedor.index', compact('fornecedores'));
     }
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -12,15 +12,159 @@
         <br>
         @switch($fornecedores[$i]['ddd'])
             @case('11')
-                Sao Paulo - SP
+                São Paulo - SP
+            @break
+
+            @case('21')
+                Rio de Janeiro - RJ
+            @break
+
+            @case('31')
+                Belo Horizonte - MG
+            @break
+
+            @case('41')
+                Curitiba - PR
+            @break
+
+            @case('51')
+                Porto Alegre - RS
+            @break
+
+            @case('61')
+                Brasília - DF
+            @break
+
+            @case('71')
+                Salvador - BA
+            @break
+
+            @case('81')
+                Recife - PE
             @break
 
             @case('85')
                 Fortaleza - CE
             @break
 
-            @case('32')
-                Minas Gerais - MG
+            @case('91')
+                Belém - PA
+            @break
+
+            @case('62')
+                Goiânia - GO
+            @break
+
+            @case('82')
+                Maceió - AL
+            @break
+
+            @case('92')
+                Manaus - AM
+            @break
+
+            @case('95')
+                Boa Vista - RR
+            @break
+
+            @case('96')
+                Macapá - AP
+            @break
+
+            @case('98')
+                São Luís - MA
+            @break
+
+            @case('99')
+                Imperatriz - MA
+            @break
+
+            @case('63')
+                Palmas - TO
+            @break
+
+            @case('64')
+                Rio Verde - GO
+            @break
+
+            @case('65')
+                Cuiabá - MT
+            @break
+
+            @case('66')
+                Rondonópolis - MT
+            @break
+
+            @case('67')
+                Campo Grande - MS
+            @break
+
+            @case('68')
+                Rio Branco - AC
+            @break
+
+            @case('69')
+                Porto Velho - RO
+            @break
+
+            @case('73')
+                Ilhéus - BA
+            @break
+
+            @case('74')
+                Juazeiro - BA
+            @break
+
+            @case('75')
+                Feira de Santana - BA
+            @break
+
+            @case('77')
+                Vitória da Conquista - BA
+            @break
+
+            @case('79')
+                Aracaju - SE
+            @break
+
+            @case('83')
+                João Pessoa - PB
+            @break
+
+            @case('84')
+                Natal - RN
+            @break
+
+            @case('86')
+                Teresina - PI
+            @break
+
+            @case('87')
+                Petrolina - PE
+            @break
+
+            @case('88')
+                Sobral - CE
+            @break
+
+            @case('89')
+                Picos - PI
+            @break
+
+            @case('93')
+                Santarém - PA
+            @break
+
+            @case('94')
+                Marabá - PA
+            @break
+
+            @case('97')
+                Tefé - AM
+            @break
+
+            @case('99')
+                Balsas - MA
             @break
 
             @default
